969ee52bf1a43c24c362d67d60d4e93e661dda2f
Egor <cowboy.killerrr@gmail.com>
2023-05-28 23:49:03 +1000

    fix: провести работу над ошибками

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,52 @@
-// ----------------------------------------
-// ОТРИСОВКА КАРТОЧЕК ПРИ ЗАГРУЗКЕ СТРАНИЦЫ
-// ----------------------------------------
-const galleryCards = [
-  {
-    name: 'Вулкан Фудзияма',
-    link: 'https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Париж',
-    link: 'https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
-  },
-  {
-    name: 'Золотые Ворота',
-    link: 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80'
-  },
-  {
-    name: 'Мормон Роу',
-    link: 'https://images.unsplash.com/photo-1610687660051-9fe41058f9b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80'
-  },
-  {
-    name: 'Озеро Комо',
-    link: 'https://images.unsplash.com/photo-1606521276375-5fbc52665c8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Сильверлейк',
-    link: 'https://images.unsplash.com/photo-1517438020812-01a8eeb72f69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
-  },
-];
-
-const galleryList = document.querySelector('.gallery__list');
+// ДЛЯ КАРТОЧЕК
+const cardsContainer = document.querySelector('.gallery__list');
 const cardTemplate = document.querySelector('#card-template');
 const cardContent = cardTemplate.content;
 const card = cardContent.querySelector('.card');
 
-galleryCards.forEach(item => {
-  const newCard = renderCards(item);
-  galleryList.prepend(newCard);
-})
+// СПИСОК МОДАЛЬНЫХ ОКОН
+const popupNodeList = document.querySelectorAll('.popup');
 
-function renderCards(item) {
+// ДЛЯ ОКНА РЕДАКТИРОВАНИЯ ПРОФИЛЯ
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const editProfileBtn = document.querySelector('.profile__btn_type_edit');
+const popupEditProfile = document.querySelector('#popup-edit-profile');
+const editProfileForm = document.querySelector('#edit-profile-form');
+const inputName = editProfileForm.querySelector('#name');
+const inputJob = editProfileForm.querySelector('#job');
+
+// ДЛЯ ОКНА ДОБАВЛЕНИЯ КАРТОЧКИ
+const addCardBtn = document.querySelector('.profile__btn_type_add');
+const popupAddCard = document.querySelector('#popup-add-card');
+const addCardForm = document.querySelector('#add-card-form');
+const inputTitle = addCardForm.querySelector('#title');
+const inputLink = addCardForm.querySelector('#link');
+
+// ДЛЯ ОКНА ПРОСМОТРА КАРТИНКИ
+const popupViewImage = document.querySelector('#popup-view-image');
+const popupImage = document.querySelector('.popup__image');
+const popupImageTitle = document.querySelector('.popup__image-subtext');
+
+// -----------------------------
+// ДОБАВИТЬ КАРТОЧКУ НА СТРАНИЦУ
+// -----------------------------
+function addNewCard(card) {
+  cardsContainer.prepend(card);
+}
+
+// ------------------
+// ОТРИСОВКА КАРТОЧЕК
+// ------------------
+function createCards(cardData) {
   const newCard = card.cloneNode(true);
 
-  const newCardImageSrc = newCard.querySelector('.card__image');
-  newCardImageSrc.src = item.link;
-
-  const newCardImageAlt = newCard.querySelector('.card__image');
-  newCardImageAlt.alt = item.name;
+  const newCardImage = newCard.querySelector('.card__image');
+  newCardImage.src = cardData.link;
+  newCardImage.alt = cardData.name;
 
   const newCardTitle = newCard.querySelector('.card__title');
-  newCardTitle.textContent = item.name;
+  newCardTitle.textContent = cardData.name;
 
   // -------------
   // ЛАЙК КАРТОЧКИ
@@ -56,11 +54,7 @@ function renderCards(item) {
   const likeBtn = newCard.querySelector('.card__like-btn');
   likeBtn.addEventListener('click', (event) => {
     const element = event.target;
-    if (element.classList.contains('card__like-btn_active')) {
-      element.classList.remove('card__like-btn_active');
-    } else {
-      element.classList.add('card__like-btn_active')
-    }
+    element.classList.toggle('card__like-btn_active');
   })
 
   // -----------------
@@ -68,59 +62,58 @@ function renderCards(item) {
   // -----------------
   const deleteBtn = newCard.querySelector('.card__delete-btn');
   deleteBtn.addEventListener('click', () => {
-    galleryList.removeChild(newCard);
+    cardsContainer.removeChild(newCard);
   })
 
   // --------------------------
   // ПРОСМОТР КАРТИНКИ КАРТОЧКИ
   // --------------------------
   const cardImage = newCard.querySelector('.card__image');
-  const popupViewImage = document.querySelector('#popup-view-image');
 
   cardImage.addEventListener('click', () => {
     openPopup(popupViewImage);
 
-    const image = popupViewImage.querySelector('.popup__image');
-    image.src = item.link;
-    image.alt = item.name;
+    popupImage.src = cardData.link;
+    popupImage.alt = cardData.name;
 
-    const imageTitle = popupViewImage.querySelector('.popup__image-subtext');
-    imageTitle.textContent = item.name;
+    popupImageTitle.textContent = cardData.name;
   })
 
   return newCard;
 }
 
-
-
-
-
-// ----------------------------------
-// ОТКРЫТИЕ И ЗАКРЫТИЕ МОДАЛЬНЫХ ОКОН
-// ----------------------------------
-
+// -----------------------
+// ОТКРЫТИЕ МОДАЛЬНЫХ ОКОН
+// -----------------------
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+}
+
+// -----------------------
+// ЗАКРЫТИЕ МОДАЛЬНЫХ ОКОН
+// -----------------------
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// ----------------------------------------
+// ОТРИСОВКА КАРТОЧЕК ПРИ ЗАГРУЗКЕ СТРАНИЦЫ
+// ----------------------------------------
+galleryCards.forEach(item => {
+  const newCard = createCards(item);
+  addNewCard(newCard);
+})
+
+// -------------------------------
+// КНОПКА ЗАКРЫТИЯ МОДАЛЬНОГО ОКНА
+// -------------------------------
+popupNodeList.forEach(popup => {
   const closeBtn = popup.querySelector('.popup__btn_type_close');
 
   closeBtn.addEventListener('click', () => {
     closePopup(popup);
   })
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-
-
-
-
-// ОКНО РЕДАКТИРОВАНИЯ ПРОФИЛЯ
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-const editProfileBtn = document.querySelector('.profile__btn_type_edit');
-const popupEditProfile = document.querySelector('#popup-edit-profile');
+})
 
 // -----------------------------------------------
 // ОТКРЫТИЕ МОДАЛЬНОГО ОКНА РЕДАКТИРОВАНИЯ ПРОФИЛЯ
@@ -128,39 +121,9 @@ const popupEditProfile = document.querySelector('#popup-edit-profile');
 editProfileBtn.addEventListener('click', () => {
   openPopup(popupEditProfile);
 
-  const editProfileForm = popupEditProfile.querySelector('#edit-profile-form');
-
-  const inputName = editProfileForm.querySelector('#name');
   inputName.value = profileName.textContent;
-
-  const inputJob = editProfileForm.querySelector('#job');
   inputJob.value = profileJob.textContent;
 })
-
-// -----------------------------------
-// ЛОГИКА ФОРМЫ РЕДАКТИРОВАНИЯ ПРОФИЛЯ
-// -----------------------------------
-const editProfileForm = document.querySelector('#edit-profile-form');
-
-editProfileForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const inputName = editProfileForm.querySelector('#name');
-  profileName.textContent = inputName.value;
-
-  const inputJob = editProfileForm.querySelector('#job');
-  profileJob.textContent = inputJob.value;
-
-  closePopup(popupEditProfile);
-})
-
-
-
-
-
-// ОКНО ДОБАВЛЕНИЯ КАРТОЧКИ
-const addCardBtn = document.querySelector('.profile__btn_type_add');
-const popupAddCard = document.querySelector('#popup-add-card');
 
 // --------------------------------------------
 // ОТКРЫТИЕ МОДАЛЬНОГО ОКНА ДОБАВЛЕНИЯ КАРТОЧКИ
@@ -169,28 +132,32 @@ addCardBtn.addEventListener('click', () => {
   openPopup(popupAddCard);
 })
 
-// --------------------------------
-// ЛОГИКА ФОРМЫ ДОБАВЛЕНИЯ КАРТОЧКИ
-// --------------------------------
-const addCardForm = document.querySelector('#add-card-form');
+// ----------------------------
+// ФОРМА РЕДАКТИРОВАНИЯ ПРОФИЛЯ
+// ----------------------------
+editProfileForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+
+  closePopup(popupEditProfile);
+})
+
+// -------------------------
+// ФОРМА ДОБАВЛЕНИЯ КАРТОЧКИ
+// -------------------------
 addCardForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const inputTitle = addCardForm.querySelector('#title');
-  const inputTitleValue = inputTitle.value;
-
-  const inputLink = addCardForm.querySelector('#link');
-  const inputLinkValue = inputLink.value;
-
   const cardData = {
-    name: inputTitleValue,
-    link: inputLinkValue,
+    name: inputTitle.value,
+    link: inputLink.value,
   }
 
-  const newCard = renderCards(cardData);
+  const newCard = createCards(cardData);
 
-  galleryList.prepend(newCard);
   addCardForm.reset();
+  addNewCard(newCard);
   closePopup(popupAddCard);
 })
