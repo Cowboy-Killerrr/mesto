4ba1d95bcb5d349b0a30a5d12aa643c4d1fba294
Egor Sinko <cowboy.killerrr@gmail.com>
2023-08-04 21:27:24 +1000

    fix: внесены правки в класс Api

--- components/Api.js
+++ components/Api.js
@@ -4,22 +4,21 @@ export default class Api {
     this.token = token;
   }
 
+  _getResponseData(response) {
+    if (!response.ok) {
+      return Promise.reject(`Ошибка: ${response.status}`);
+    }
+
+    return response.json();
+  }
+
   getUserDataObj() {
     return fetch(`${this.url}/users/me`, {
       headers: {
         authorization: this.token
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(`Ошибка: ${response.status}`);
-        }
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this._getResponseData(response))
   }
 
   getInitialCards() {
@@ -28,16 +27,7 @@ export default class Api {
         authorization: this.token
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(`Ошибка: ${response.status}`);
-        }
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this._getResponseData(response))
   }
 
   editUserInfo(userDataObj) {
@@ -49,9 +39,6 @@ export default class Api {
       },
       body: JSON.stringify(userDataObj)
     })
-      .catch((err) => {
-        console.log(err);
-      });
   }
 
   editUserAvatar(userAvatarObj) {
@@ -63,9 +50,6 @@ export default class Api {
       },
       body: JSON.stringify(userAvatarObj)
     })
-      .catch((err) => {
-        console.log(err);
-      });
   }
 
   addNewCard(cardDataObj) {
@@ -77,9 +61,6 @@ export default class Api {
       },
       body: JSON.stringify(cardDataObj)
     })
-      .catch((err) => {
-        console.log(err);
-      });
   }
 
   deleteCard(id) {
@@ -89,16 +70,7 @@ export default class Api {
         authorization: this.token
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(`Ошибка: ${response.status}`);
-        }
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this._getResponseData(response))
   }
 
   likeCard(id) {
@@ -108,16 +80,7 @@ export default class Api {
         authorization: this.token
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(`Ошибка: ${response.status}`);
-        }
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this._getResponseData(response))
   }
 
   unlikeCard(id) {
@@ -127,15 +90,6 @@ export default class Api {
         authorization: this.token
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(`Ошибка: ${response.status}`);
-        }
-
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this._getResponseData(response))
   }
 }
